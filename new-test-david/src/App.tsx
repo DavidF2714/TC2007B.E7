4037392e9eb6c4a5b6703f553ba9c6bf72c6672c
IanVazquez <A01027225@tec.mx>
2023-10-14 12:07:13 -0600

    users

--- new-test-david/src/App.tsx
+++ new-test-david/src/App.tsx
@@ -9,25 +9,15 @@ import Registrarse from "./testregister";
 import { Route } from "react-router-dom";
 import authProvider from "./AuthProvider";
 import CustomLoginPage from "./testlogin";
+import { UserList } from "./Users";
 
 export const App = () => (
     <Admin dataProvider={dataProvider} authProvider={authProvider} layout={MyLayout} darkTheme={defaultDarkTheme} theme={defaultLightTheme} defaultTheme="light" i18nProvider={i18nProvider} loginPage={CustomLoginPage}>
-        <Resource name="dashboard" list={Dashboard} show={Dashboard} options={{label:'Dashboard'}}/>
         <Resource name="tickets" list={TicketList} edit={TicketEdit} create={TicketCreate} options={{label:'Tickets'}} />
+        <Resource name="dashboard" list={Dashboard} show={Dashboard} options={{label:'Dashboard'}}/>
+        <Resource name="usuarios" list ={UserList} options={{label:'UserList'}}/>
         <CustomRoutes noLayout>
             <Route path="/registrarse" element={<Registrarse />}/>
         </CustomRoutes>
-        <CustomRoutes>
-            {(permissions) => {
-                // Verifica los permisos del usuario y oculta la ruta de "dashboard" si es necesario
-                if (permissions === 'Coordinador') {
-                    return null; // No mostrar la ruta de "dashboard"
-                } else {
-                    // Muestra la ruta de "dashboard" para otros usuarios
-                    return <Resource name="dashboard" options={{ label: 'Dashboard' }} />;
-                }
-            }}
-        </CustomRoutes>
     </Admin>
 ); 
-
