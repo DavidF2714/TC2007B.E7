6d25e82377d0cfb90fea08857167a963cd8c2e4c
David Vieyra <davidsvgd@hotmail.com>
2023-10-17 18:30:25 -0600

    TICKETS 80%

--- new-test-david/src/Components/MuiCard.jsx
+++ new-test-david/src/Components/MuiCard.jsx
@@ -1,22 +1,94 @@
-import {Box, Card, CardContent, Typography, Button} from '@mui/material'
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-export const MuiCard = () => {
-  return <Box width = '300px'>
-    <Card variant="outlined" style={{ marginBottom: "1rem" }}>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          Ticket #
-        </Typography>
-        <Typography color="text.secondary">Aula: </Typography>
-        <Typography color="text.secondary">Coordinador: </Typography>
-        {/* Add other fields as needed */}
-        <Button variant="contained">
-          Edit
-        </Button>
-        <Button variant="contained">
-          Delete
-        </Button>
-      </CardContent>
-    </Card>
-    </Box>
-}
+export const MuiCard = (props) => {
+  return (
+    <div>
+      {props.details.map((detail, i) => (
+        <Card sx={{ maxWidth: 345 }} key={i}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {detail.aula}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {detail.prioridad}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {detail.coordinador}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+// import { Box, Card, CardContent, Typography, Button } from "@mui/material";
+// import { useDataProvider } from "react-admin";
+// //import { useState, useEffect } from "react";
+// import React, { useState } from 'react';
+
+// export const MuiCard = () => {
+//   const dataProvider = useDataProvider();
+//   const [tickets, setTickets] = useState<any>([]);
+
+//   useEffect(() => {
+//     dataProvider
+//       .getList("tickets", {
+//         pagination: { page: 1, perPage: 1 },
+//         sort: { field: "id", order: "ASC" },
+//         filter: {},
+//       })
+//       .then((response) => setTickets(response.data))
+//       //.then((data) => setTickets(data))
+//       .catch((error) => console.error("Error:al obtener los tickets", error));
+//   }, []);
+
+//   return (
+//     <Box
+//       display="flex"
+//       justifyContent="space-between"
+//       alignItems="center"
+//       borderBottom={`4px solid`}
+//       p="15px"
+//     >
+//       {tickets.map((ticket, i) => (
+//         <Box
+//           key={`ticket-${i}`}
+//           display="flex"
+//           justifyContent="space-between"
+//           alignItems="center"
+//           borderBottom={`4px solid`}
+//           p="15px"
+//         >
+//           <Box>
+//             <Typography
+//               variant="h5"
+//               fontWeight="600"
+//             >
+//               {ticket.aula}
+//             </Typography>
+//             <Typography>
+//               {ticket.coordinador}
+//             </Typography>
+//           </Box>
+//           <Box></Box>
+//           <Box p="5px 10px" borderRadius="4px">
+//             {ticket.estado}
+//           </Box>
+//         </Box>
+//       ))}
+//     </Box>
+//   );
+// };
+
+// //export default Tickets;
--- new-test-david/src/tickets.tsx
+++ new-test-david/src/tickets.tsx
@@ -1,4 +1,4 @@
-import { 
+import {
   Datagrid,
   List,
   TextField,
@@ -12,57 +12,175 @@ import {
   useNotify,
   SaveButton,
   useDataProvider,
-  Show
-} from 'react-admin';
-import { Box, Container, Grid, Paper, ThemeProvider, createTheme} from '@mui/material';
-
+} from "react-admin";
+import {
+  Box,
+  Container,
+  Grid,
+  Paper,
+  ThemeProvider,
+  createTheme,
+  useTheme,
+} from "@mui/material";
 import React, { useState, useEffect } from "react";
-import  Header  from './Components/Header.jsx';
+import Header from "./Components/Header.jsx";
 import { Route } from "react-router-dom";
-import { required } from 'ra-core';
-import { MuiCard } from './Components/MuiCard.jsx';
+import { required } from "ra-core";
+import { MuiCard } from "./Components/MuiCard.jsx";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+} from "@mui/material";
+import { tokens } from "./theme.js";
 
-export const TicketList = () => (
-  <Box m="1.5rem 2.5rem">
-    <Header title="TICKETS" subtitle="Ver lista de tickets."/>
-    <List>
+export const TicketList = () => {
+  const dataProvider = useDataProvider();
+  const [tickets, setTickets] = useState<any[]>([]);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  useEffect(() => {
+    dataProvider
+      .getList("tickets", {
+        pagination: { page: 1, perPage: 100 },
+        sort: { field: "id", order: "ASC" },
+        filter: {},
+      })
+      .then((response) => {
+        setTickets(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los tickets", error);
+      });
+  }, []);
+
+  return (
+    <Box gridColumn="span 4" gridRow="span 2" overflow="auto">
+<List>
     <Datagrid rowClick="edit">
       <TextField source='aula' label="Aula" />
-      <TextField source="coordinador" label="Coordinador" />
-      <TextField source='folio' label="Folio" />
-      <TextField source="categoria" label="Categoría" />
-      <TextField source="subcategoria" label="Subcategoría" />
-      <TextField source="estado" label="Estado" />
-      <TextField source='prioridad' label="Prioridad"/>
-      <TextField source="timestamp" label="Fecha y Hora" />
+      {/* ... other fields */}
     </Datagrid>
-    </List> 
-    <MuiCard></MuiCard>
+  </List>
+    <Header title="TICKETS" subtitle="Ver lista de tickets."/>
+    {tickets.map((ticket, i) => (
+      <Card 
+      sx={{ maxWidth: 345 }} 
+      key={`ticket-${i}`}>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {ticket.aula}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Prioridad: {ticket.prioridad}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {ticket.coordinador}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Mostrar más</Button>
+                <Button size="small"></Button>
+                <Button color={"error"} size="small" startIcon={<DeleteIcon />}>Borrar</Button>
+              </CardActions>
+            </Card>
+    ))}
   </Box>
-);
+    );
+    };
+
+// const TicketList = () => {
+//   const [tickets, setTickets] = useState<any[]>([]);
+
+//   // Call TicketData with setTickets as a prop
+//   TicketData({ setTickets });
+
+//   return (
+// <Box m="1.5rem 2.5rem">
+//   <Header title="TICKETS" subtitle="Ver lista de tickets."/>
+  // <List>
+  //   <Datagrid rowClick="edit">
+  //     <TextField source='aula' label="Aula" />
+  //     {/* ... other fields */}
+  //   </Datagrid>
+  // </List>
+//   <MuiCard details={tickets} /> {/* Pass tickets as details */}
+// </Box>
+//   );
+// };
+
+// const TicketData = () => {
+//   const [tickets, setTickets] = useState<any[]>([]);
+
+//   useEffect(() => {
+//     dataProvider
+//       .getList("tickets", {
+//         pagination: { page: 1, perPage: 100 },
+//         sort: { field: "id", order: "ASC" },
+//         filter: {},
+//       })
+//       .then((response) => {
+//         setTickets(response.data);
+//       })
+//       .catch((error) => {
+//         console.error("Error al obtener los tickets", error);
+//       });
+//   }, []);
+// };
+
+// export const TicketList = () => (
+//   <Box m="1.5rem 2.5rem">
+//     <Header title="TICKETS" subtitle="Ver lista de tickets."/>
+//     <List>
+//     <Datagrid rowClick="edit">
+//       <TextField source='aula' label="Aula" />
+//       <TextField source="coordinador" label="Coordinador" />
+//       <TextField source='folio' label="Folio" />
+//       <TextField source="categoria" label="Categoría" />
+//       <TextField source="subcategoria" label="Subcategoría" />
+//       <TextField source="estado" label="Estado" />
+//       <TextField source='prioridad' label="Prioridad"/>
+//       <TextField source="timestamp" label="Fecha y Hora" />
+//     </Datagrid>
+//     </List>
+//     <MuiCard />
+//   </Box>
+// );
 
 export const TicketEdit: React.FC = (props) => {
   const { permissions } = usePermissions();
 
   // Verificar si el usuario tiene permisos de Coordinador
-  const isCoordinador = permissions.includes('Coordinador');
+  const isCoordinador = permissions.includes("Coordinador");
 
   return (
     <Edit {...props}>
-      <SimpleForm warnWhenUnsavedChanges toolbar={<SaveButton label="Guardar"/>}>
+      <SimpleForm
+        warnWhenUnsavedChanges
+        toolbar={<SaveButton label="Guardar" />}
+      >
         <TextInput source="aula" label="Aula" disabled validate={required()} />
         <TextInput source="coordinador" disabled validate={required()} />
         <TextInput source="categoria" disabled />
         <TextInput source="folio" disabled label="Número de Oficio" />
         <TextInput source="subcategoria" disabled />
-        <TextInput source="timestamp" label="Fecha y Hora" disabled validate={required()} />
+        <TextInput
+          source="timestamp"
+          label="Fecha y Hora"
+          disabled
+          validate={required()}
+        />
         <SelectInput
           validate={required()}
           source="estado"
           label="Estado"
           choices={[
-            { id: 'En curso', name: 'En Curso' },
-            { id: 'Completado', name: 'Completado' }
+            { id: "En curso", name: "En Curso" },
+            { id: "Completado", name: "Completado" },
           ]}
         />
         <SelectInput
@@ -70,9 +188,9 @@ export const TicketEdit: React.FC = (props) => {
           source="prioridad"
           label="Prioridad"
           choices={[
-            { id: 'Bajo', name: 'Bajo' },
-            { id: 'Medio', name: 'Media' },
-            { id: 'Alto', name: 'Alto' }
+            { id: "Bajo", name: "Bajo" },
+            { id: "Medio", name: "Media" },
+            { id: "Alto", name: "Alto" },
           ]}
           disabled={isCoordinador}
         />
@@ -83,84 +201,89 @@ export const TicketEdit: React.FC = (props) => {
   );
 };
 
-type SubCategory = { id: string; name: string;};
+type SubCategory = { id: string; name: string };
 type SubCategoriesOptions = Record<string, SubCategory[]>;
 
 const subCategoriesOptions: SubCategoriesOptions = {
-    'Servicios':[
-          { id: 'Agua', name:'Agua'},
-          { id: 'Luz', name: 'Luz'},
-          { id: 'Telefono', name: 'Telefono'},
-          { id: 'Basura', name: 'Basura'},
-          { id: 'Limpieza del Aula', name: 'Limpieza del Aula'}
-        ],
-    'Digital':[
-          { id: 'Internet/Servidores/Equipo', name:'Internet/Servidores/Equipo'},
-          { id: 'Software', name:'Software'},
-          { id: 'Hardware', name:'Hardware'},
-          { id: 'Cámara de Seguridad', name:'Cámara de Seguridad'},
-          { id: 'Soporte Técnico Presencial y Remoto', name:'Soporte Técnico Presencial y Remoto'},
-        ],
-    'Infraestructura':[
-          { id: 'Paredes', name:'Paredes'},
-          { id: 'Techos', name:'Techos'},
-          { id: 'Ventanas', name:'Ventanas'},
-          { id: 'Puertas', name:'Puertas'},
-          { id: 'Aulas en general', name:'Aulas en general'}
-        ],
-     'Recursos Humanos':[
-          { id: 'Permisos', name:'Permisos'},
-          { id: 'Asistencias', name:'Asistencias'},
-          { id: 'Salud', name:'Salud'},
-          { id: 'Tramites', name:'Tramites'},
-          { id: 'Honorarios', name:'Honorarios'},
-        ],
-    'Beneficiarios':[
-          { id: 'Asistencias', name:'Asistencias'},
-          { id: 'Documentacion', name:'Documentacion'},
-          { id: 'Apoyo Academico', name:'Apoyo Academico'},
-          { id: 'Salud', name:'Salud'},
-          { id: 'Seguridad, Bullying', name:'Seguridad, Bullying' },
-        ],
-    'Mobiliario':[
-          { id: 'Sillas, Butacas', name:'Sillas, Butacas' },
-          { id: 'Escritorios', name:'Escritorios'},
-          { id: 'Pizarrones', name:'Pizarrones'},
-          { id: 'Cafetería', name:'Cafetería'},
-          { id: 'Estantes, Archiveros', name:'Estantes, Archiveros' },
-        ],
-    'Seguridad':[
-          { id: 'Delicuencia', name:'Delicuencia'},
-          { id: 'Robo', name:'Robo' },
-          { id: 'Bandalismo', name:'Bandalismo'},
-          { id: 'Imagen Intistucional', name:'Imagen Intistucional'},
-        ],
-    'Materiales':[
-          { id: 'Educativos', name:'Educativos'},
-          { id: 'Papeleria', name:'Papeleria' },
-          { id: 'Limpieza', name:'Limpieza'},
-        ],
-    'Fenómeno Metereológico':[
-          { id: 'Inundaciones', name:'Inundaciones'},
-          { id: 'Incendios', name:'Incendios' },
-          { id: 'Sismos', name:'Sismos' },
-        ]
-        };
+  Servicios: [
+    { id: "Agua", name: "Agua" },
+    { id: "Luz", name: "Luz" },
+    { id: "Telefono", name: "Telefono" },
+    { id: "Basura", name: "Basura" },
+    { id: "Limpieza del Aula", name: "Limpieza del Aula" },
+  ],
+  Digital: [
+    { id: "Internet/Servidores/Equipo", name: "Internet/Servidores/Equipo" },
+    { id: "Software", name: "Software" },
+    { id: "Hardware", name: "Hardware" },
+    { id: "Cámara de Seguridad", name: "Cámara de Seguridad" },
+    {
+      id: "Soporte Técnico Presencial y Remoto",
+      name: "Soporte Técnico Presencial y Remoto",
+    },
+  ],
+  Infraestructura: [
+    { id: "Paredes", name: "Paredes" },
+    { id: "Techos", name: "Techos" },
+    { id: "Ventanas", name: "Ventanas" },
+    { id: "Puertas", name: "Puertas" },
+    { id: "Aulas en general", name: "Aulas en general" },
+  ],
+  "Recursos Humanos": [
+    { id: "Permisos", name: "Permisos" },
+    { id: "Asistencias", name: "Asistencias" },
+    { id: "Salud", name: "Salud" },
+    { id: "Tramites", name: "Tramites" },
+    { id: "Honorarios", name: "Honorarios" },
+  ],
+  Beneficiarios: [
+    { id: "Asistencias", name: "Asistencias" },
+    { id: "Documentacion", name: "Documentacion" },
+    { id: "Apoyo Academico", name: "Apoyo Academico" },
+    { id: "Salud", name: "Salud" },
+    { id: "Seguridad, Bullying", name: "Seguridad, Bullying" },
+  ],
+  Mobiliario: [
+    { id: "Sillas, Butacas", name: "Sillas, Butacas" },
+    { id: "Escritorios", name: "Escritorios" },
+    { id: "Pizarrones", name: "Pizarrones" },
+    { id: "Cafetería", name: "Cafetería" },
+    { id: "Estantes, Archiveros", name: "Estantes, Archiveros" },
+  ],
+  Seguridad: [
+    { id: "Delicuencia", name: "Delicuencia" },
+    { id: "Robo", name: "Robo" },
+    { id: "Bandalismo", name: "Bandalismo" },
+    { id: "Imagen Intistucional", name: "Imagen Intistucional" },
+  ],
+  Materiales: [
+    { id: "Educativos", name: "Educativos" },
+    { id: "Papeleria", name: "Papeleria" },
+    { id: "Limpieza", name: "Limpieza" },
+  ],
+  "Fenómeno Metereológico": [
+    { id: "Inundaciones", name: "Inundaciones" },
+    { id: "Incendios", name: "Incendios" },
+    { id: "Sismos", name: "Sismos" },
+  ],
+};
 
 export const TicketCreate: React.FC = (props) => {
-  const {permissions} = usePermissions();
-  const isCoordinador = permissions.includes('Coordinador');
+  const { permissions } = usePermissions();
+  const isCoordinador = permissions.includes("Coordinador");
   const notify = useNotify();
 
-  const [selectedCategory, setSelectedCategory] = useState('1');
-  const [subCategories, setSubCategories] = useState(subCategoriesOptions[selectedCategory] || []);
+  const [selectedCategory, setSelectedCategory] = useState("1");
+  const [subCategories, setSubCategories] = useState(
+    subCategoriesOptions[selectedCategory] || []
+  );
   useEffect(() => {
-  setSubCategories(subCategoriesOptions[selectedCategory] || []);
+    setSubCategories(subCategoriesOptions[selectedCategory] || []);
   }, [selectedCategory]);
   const currentTimestamp = new Date();
 
   const identity = JSON.parse(localStorage.getItem("identity") || "{}");
-  const fullName = identity ? identity.fullName : '';
+  const fullName = identity ? identity.fullName : "";
   const formattedTimestamp = currentTimestamp.toLocaleString("en-US", {
     year: "numeric",
     month: "2-digit",
@@ -169,51 +292,85 @@ export const TicketCreate: React.FC = (props) => {
     minute: "2-digit",
   });
 
-  if(!isCoordinador){
-    notify('No tiene los permisos para crear un ticket',{type:'error'});
-    window.location.href="/#/tickets"
+  if (!isCoordinador) {
+    notify("No tiene los permisos para crear un ticket", { type: "error" });
+    window.location.href = "/#/tickets";
     return null;
   }
-
 
   return (
     <Create {...props}>
       <SimpleForm warnWhenUnsavedChanges>
-        <DateInput source='timestamp' label="Fecha" defaultValue={formattedTimestamp} disabled validate={required()} sx={{ justifyContent: 'center' }}/>
+        <DateInput
+          source="timestamp"
+          label="Fecha"
+          defaultValue={formattedTimestamp}
+          disabled
+          validate={required()}
+          sx={{ justifyContent: "center" }}
+        />
         <TextInput source="aula" label="Aula" validate={required()} />
-        <TextInput source="coordinador" label="Nombre del Coordinador" defaultValue={fullName} type='text' disabled validate={required()} />
-        <TextInput source='folio' label="Número de Oficio" />
-        <SelectInput validate={required()}
-          source='categoria'
-          label='Categoría'
+        <TextInput
+          source="coordinador"
+          label="Nombre del Coordinador"
+          defaultValue={fullName}
+          type="text"
+          disabled
+          validate={required()}
+        />
+        <TextInput source="folio" label="Número de Oficio" />
+        <SelectInput
+          validate={required()}
+          source="categoria"
+          label="Categoría"
           choices={[
-          { id: 'Servicios', name: 'Servicios'},
-          { id: 'Digital', name: 'Digital'},
-          { id: 'Infraestructura',name: 'Infraestructura'},
-          { id: 'Recursos Humanos',name: 'Recursos humanos'},
-          { id: 'Beneficiarios', name:'Beneficiario'},
-          { id: 'Mobiliario', name: 'Mobiliario'},
-          { id: 'Seguridad', name:'Seguridad'},
-          { id: 'Materiales', name: 'Materiales'},
-          { id: 'Fenómeno Metereológico', name:'Fenómeno Metereológico'}
-        ]} onChange={(event) => {
-          const newValue = event.target.value;
-          console.log(event.target.value);
-          setSelectedCategory(newValue);
-        }}/>
+            { id: "Servicios", name: "Servicios" },
+            { id: "Digital", name: "Digital" },
+            { id: "Infraestructura", name: "Infraestructura" },
+            { id: "Recursos Humanos", name: "Recursos humanos" },
+            { id: "Beneficiarios", name: "Beneficiario" },
+            { id: "Mobiliario", name: "Mobiliario" },
+            { id: "Seguridad", name: "Seguridad" },
+            { id: "Materiales", name: "Materiales" },
+            { id: "Fenómeno Metereológico", name: "Fenómeno Metereológico" },
+          ]}
+          onChange={(event) => {
+            const newValue = event.target.value;
+            console.log(event.target.value);
+            setSelectedCategory(newValue);
+          }}
+        />
         <SelectInput
           validate={required()}
           source="subcategoria"
-          label='Subcategoría'
+          label="Subcategoría"
           choices={subCategories}
         />
-        <TextInput validate={required()} source='estado' label="Estado" defaultValue="Nuevo" disabled/>
-        <SelectInput validate={required()} source='prioridad' label="Prioridad" choices={[
-          { id: 'Bajo', name: 'Bajo' },
-          { id: 'Medio', name: 'Media' },
-          { id: 'Alto', name: 'Alto' }
-        ]} />
-        <TextInput source="descripcion" multiline rows={5} label="Descripción" fullWidth validate={required()} />
+        <TextInput
+          validate={required()}
+          source="estado"
+          label="Estado"
+          defaultValue="Nuevo"
+          disabled
+        />
+        <SelectInput
+          validate={required()}
+          source="prioridad"
+          label="Prioridad"
+          choices={[
+            { id: "Bajo", name: "Bajo" },
+            { id: "Medio", name: "Media" },
+            { id: "Alto", name: "Alto" },
+          ]}
+        />
+        <TextInput
+          source="descripcion"
+          multiline
+          rows={5}
+          label="Descripción"
+          fullWidth
+          validate={required()}
+        />
       </SimpleForm>
     </Create>
   );
