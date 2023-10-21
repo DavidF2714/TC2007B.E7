fab1fb9e9f1dc8a328f2a6640df7b009d060c253
David Flores Becerril <90709883+DavidF2714@users.noreply.github.com>
2023-10-20 22:46:02 -0600

    Tickets Alpha 1.0

---
+++ backend/adminCreate.js
@@ -0,0 +1,67 @@
+const { MongoClient } = require("mongodb");
+const bcrypt = require("bcrypt");
+
+// URL de conexión a la base de datos
+const mongoURL = "mongodb://127.0.0.1:27017/tc2007b";
+
+// Datos del usuario que deseas agregar
+const id = 1
+const usuario = "ejecu@hotmail.com";
+const password = "ejecu";
+const fullName = "Neto Fernández";
+const permissions = 'Ejecutivo';
+
+// Número de rondas de salto para bcrypt
+const saltRounds = 10;
+
+
+
+// Función asincrónica para conectar a la base de datos
+async function connectDB() {
+  try {
+    const client = new MongoClient(mongoURL, {
+      
+    });
+    await client.connect();
+    const db = client.db("tc2007b"); // Reemplaza "tc2007b" con el nombre de tu base de datos
+    console.log("Conectado a la base de datos");
+    return db;
+  } catch (error) {
+    console.error("Error al conectar a la base de datos:", error);
+    throw error;
+  }
+}
+
+// Función principal asincrónica
+async function main() {
+  try {
+    const db = await connectDB();
+
+    // Generar el hash de la contraseña
+    bcrypt.genSalt(saltRounds, async (error, salt) => {
+      if (error) {
+        console.error("Error al generar el hash de contraseña:", error);
+        return;
+      }
+      bcrypt.hash(password, salt, async (error, hash) => {
+        if (error) {
+          console.error("Error al generar el hash de contraseña:", error);
+          return;
+        }
+        // Insertar el usuario en la base de datos
+        const result = await db.collection("usuarios").insertOne({
+            id: id,
+          usuario: usuario,
+          password: hash,
+          fullName: fullName,
+          permissions: permissions
+        });
+        console.log("Usuario agregado con éxito.");
+      });
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+main();
